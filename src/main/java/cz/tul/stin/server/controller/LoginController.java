a1dccbe6d909e86dc1bf6bff0137579e1e3592a9
Petr Kaiser <petr.kaiser@tul.cz>
2023-04-06 12:33:48 +0200

    backend

--- src/main/java/cz/tul/stin/server/controller/LoginController.java
+++ src/main/java/cz/tul/stin/server/controller/LoginController.java
@@ -13,20 +13,8 @@ public class LoginController {
 
     @PostMapping("/login")
     public String login(@RequestBody String clientId){
-        User client;
-        try {
-            client = User.getUserData(Integer.parseInt(clientId.replace("=", "")));
-        } catch (Exception e){
-            client = null;
-        }
-        if (client != null) {
-            String code = Bank.generateRandomCode();
-            String msg = String.format("Váš kód pro přilášení je: %s", code);
-            service.sendSimpleEmail(client.getEmail(), Const.EMAIL_SUBJECT, msg);
-            return code;
-        } else {
-            return "-1";
-        }
+        return clientId.replace("=", "");
+        
     }
 
 }
