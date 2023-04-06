46c0deb510034616a870d2a32ca6c246e4f69d7e
Petr Kaiser <petr.kaiser@tul.cz>
2023-04-06 12:44:25 +0200

    backend

--- src/main/java/cz/tul/stin/server/controller/LoginController.java
+++ src/main/java/cz/tul/stin/server/controller/LoginController.java
@@ -1,5 +1,7 @@
 package cz.tul.stin.server.controller;
-
+import cz.tul.stin.server.model.*;
+import cz.tul.stin.server.config.Const;
+import cz.tul.stin.server.service.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,11 +9,25 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin("https://stellular-maamoul-21d188.netlify.app/")
 public class LoginController {
     @Autowired
-
+    private EmailSenderService service;
 
     @PostMapping("/login")
     public String login(@RequestBody String clientId){
-        return clientId.replace("=", "");
+
+        User client;
+        try {
+            client = User.getUserData(Integer.parseInt(clientId.replace("=", "")));
+        } catch (Exception e){
+            client = null;
+        }
+        if (client != null) {
+            String code = Bank.generateRandomCode();
+            String msg = String.format("Váš kód pro přilášení je: %s", code);
+            service.sendSimpleEmail(client.getEmail(), Const.EMAIL_SUBJECT, msg);
+            return code;
+        } else {
+            return "-1";
+        }
     }
 
 }
